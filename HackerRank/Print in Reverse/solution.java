f0b36424f82f0ab92562b642958da2f4aa52fb60
lsy-it-1995 <garyshaoyu@gmail.com>
2021-04-06 22:04:46 +0800

    cracking code

---
+++ HackerRank/Print in Reverse/solution.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep) {
+        while (node != null) {
+            System.out.print(node.data);
+
+            node = node.next;
+
+            if (node != null) {
+                System.out.print(sep);
+            }
+        }
+    }
+
+    // Complete the reversePrint function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static void reversePrint(SinglyLinkedListNode head) {
+        Stack<Integer> stack = new Stack<>();
+        
+        while(head != null){
+            stack.add(head.data);
+            head = head.next;
+        }
+        
+        while(!stack.isEmpty()){
+            System.out.println(stack.pop());
+        }
+        return;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
---
+++ LeetCode/easy/21. Merge Two Sorted Lists/solution2.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null){
+            return l2;
+        }
+        if(l2 == null){
+            return l1;
+        }
+        
+        ListNode merge = new ListNode(),
+                 temp = merge;
+        while(l1!=null && l2!= null){
+           if(l1.val < l2.val){
+               temp.next = l1;
+               temp = temp.next;
+               l1 = l1.next;
+           }else{
+               temp.next = l2;
+               temp = temp.next;
+               l2 = l2.next;
+           }
+        }
+        while(l1!=null){
+            temp.next = l1;
+            temp = temp.next;
+            l1 = l1.next;
+        }
+        
+        while(l2!= null){
+            temp.next = l2;
+            temp = temp.next;
+            l2 = l2.next;
+        }
+        
+        return merge.next;
+    }
+}
