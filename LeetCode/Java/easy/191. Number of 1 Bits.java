f1ab763c7ad9a1ff66146a18985c66f733db14d5
lsy-it-1995 <garyshaoyu@gmail.com>
2021-06-13 21:31:36 +0800

    add readme in leetcode

--- LeetCode/Java/easy/191. Number of 1 Bits.java
+++ LeetCode/Java/easy/191. Number of 1 Bits.java
@@ -1,13 +1,13 @@
 public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
-        int ones = 0, mask = 1;
+        int res = 0;
         for(int i = 0; i < 32; i++){
-            if((n & mask) != 0){
-                ones++;
+            if((n&1) == 1){
+                res++;
             }
-            mask <<= 1;
+            n>>=1;
         }
-        return ones;
+        return res;
     }
 }
---
+++ LeetCode/README.md
@@ -0,0 +1,7 @@
+# Java
+
+## [Easy] (https://github.com/lsy-it-1995/Problems/tree/main/LeetCode/easy)
+
+## [Medium] (https://github.com/lsy-it-1995/Problems/tree/main/LeetCode/hard)
+
+## [Hard](https://github.com/lsy-it-1995/Problems/tree/main/LeetCode/medium)
