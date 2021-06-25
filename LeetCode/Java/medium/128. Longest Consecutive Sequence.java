e3d6d4e49ac1bc715159859303c7bbf20ad8eaf4
lsy-it-1995 <garyshaoyu@gmail.com>
2021-06-25 21:02:14 +0800

    leetcode update

--- LeetCode/Java/medium/128. Longest Consecutive Sequence.java
+++ LeetCode/Java/medium/128. Longest Consecutive Sequence.java
@@ -18,3 +18,27 @@ class Solution {
         return ans;
     }
 }
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        
+        if(nums.length == 0 || nums == null)
+            return 0;
+        Arrays.sort(nums);
+        int cur = nums[0],
+            max = 1, sum = 1;
+        
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] == nums[i-1])
+                continue;
+            if(nums[i] == cur + 1){
+                cur++;
+                sum++;
+            }else{
+                cur = nums[i];
+                sum = 1;
+            }
+            max = Math.max(max, sum);
+        }
+        return max;
+}
