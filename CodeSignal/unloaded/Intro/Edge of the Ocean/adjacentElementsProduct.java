ebe1df7f712d5e0262280fcd6d0f4eaaf3e6f8e6
lsy-it-1995 <garyshaoyu@gmail.com>
2021-09-24 16:52:02 -0700

    codesignal

--- CodeSignal/unloaded/Intro/Edge of the Ocean/adjacentElementsProduct.java
+++
@@ -1,8 +0,0 @@
-int adjacentElementsProduct(int[] inputArray) {
-    int max = Integer.MIN_VALUE;
-    for(int i = 1; i < inputArray.length; i++){
-        int localmax = inputArray[i] * inputArray[i-1];
-        max = Math.max(max, localmax);
-    }
-    return max;
-}
