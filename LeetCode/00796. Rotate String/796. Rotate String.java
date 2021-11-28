fe7e4a62d8c37dc0d1e82e1d4c6aa4b27c90e656
lsy-it-1995 <garyshaoyu@gmail.com>
2021-11-28 15:45:41 -0800

    14, 701, 767, 796, 965, 977, 1008, 1232

--- LeetCode/00796. Rotate String/796. Rotate String.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public boolean rotateString(String A, String B) {
-        if(A.length() != B.length()){
-            return false;
-        }
-        StringBuilder sb = new StringBuilder(A + A);
-        return sb.indexOf(B) != -1;
-    }
-}
