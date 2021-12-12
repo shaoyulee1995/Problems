75a3e23dca44a8a96a5014412597d81c90cf39e0
lsy-it-1995 <garyshaoyu@gmail.com>
2021-12-12 01:37:19 -0800

    225, 232, 788, 874, 929, 941, 1325, 1572, 1742

--- LeetCode/00232 Implement Queue using Stacks/232. Implement Queue using Stacks.java
+++ LeetCode/00232 Implement Queue using Stacks/232. Implement Queue using Stacks.java
@@ -1,45 +1,32 @@
 class MyQueue {
 
-    Stack<Integer> s1,
-                   s2;
-    /** Initialize your data structure here. */
+    Stack<Integer> s1, s2;
+    
     public MyQueue() {
-        s1 = new Stack<Integer>();
-        s2 = new Stack<Integer>();
+        s1 = new Stack();
+        s2 = new Stack();
     }
     
-    /** Push element x to the back of queue. */
     public void push(int x) {
-        s1.add(x);
+        s1.push(x);
     }
     
-    /** Removes the element from in front of queue and returns that element. */
     public int pop() {
-        while(s1.size() > 1){
-            s2.add(s1.pop());
-        }
-        int val = s1.pop();
-        while(!s2.isEmpty()){
-            s1.add(s2.pop());
-        }
-        return val;
+        peek();
+        return s2.pop();
     }
     
-    /** Get the front element. */
     public int peek() {
-        while(s1.size() > 1){
-            s2.add(s1.pop());
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
         }
-        int val = s1.peek();
-        while(!s2.isEmpty()){
-            s1.add(s2.pop());
-        }
-        return val;
+        return s2.peek();
     }
     
-    /** Returns whether the queue is empty. */
     public boolean empty() {
-        return (s1.isEmpty());
+        return s1.isEmpty() && s2.isEmpty();
     }
 }
 
