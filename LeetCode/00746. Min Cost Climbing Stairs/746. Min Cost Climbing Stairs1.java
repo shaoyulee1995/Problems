b6caff11d3f16af7254c2cde8f6e8fbcf273d8f1
lsy-it-1995 <garyshaoyu@gmail.com>
2022-01-29 09:26:40 +0800

    62, 63, 70, 343, 509, 746, 941

--- LeetCode/00746. Min Cost Climbing Stairs/746. Min Cost Climbing Stairs1.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int minCostClimbingStairs(int[] cost) {
-        int[] dp = new int[cost.length + 1];
-        for(int i = 2; i < cost.length + 1; i++){
-            dp[i] += Math.min(dp[i-2] + cost[i-2], dp[i-1]+cost[i-1]);
-        }
-        return dp[dp.length - 1];
-    }
-}
