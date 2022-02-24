a4bb19e2d330a99ca58ee3c24a991f4998cf9b52
lsy-it-1995 <garyshaoyu@gmail.com>
2022-02-24 09:08:38 +0800

    171,852,868,872,937,938,942,953,1021,1022,1037,1351,1403,1431,1436,1450,1460,1464,1470,1475,1512,1588

--- LeetCode/01588. Sum of All Odd Length Subarrays/1588. Sum of All Odd Length Subarrays2.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int sumOddLengthSubarrays(int[] arr) {
-        int result = 0;
-        for(int i = 0; i < arr.length; i++){
-            result += ((arr.length - i)*(i + 1) + 1) / 2* arr[i];
-        }
-        return result;
-    }
-}
