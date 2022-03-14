c426f826bdc042b87982bb0778e46b69e91d0d48
lsy-it-1995 <garyshaoyu@gmail.com>
2022-03-14 19:46:15 +0800

    2,42,56,71,82,88,121,146,200,312,423,523,1630,1631,1637,1641,1647,1669,1689,1769,1814,1828

--- LeetCode/01689. Partitioning Into Minimum Number Of Deci-Binary Numbers/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int minPartitions(String n) {
-        int result = Integer.MIN_VALUE;
-        for(char c: n.toCharArray()){
-            result = Math.max(c-'0', result);
-        }
-        return result;
-    }
-}
