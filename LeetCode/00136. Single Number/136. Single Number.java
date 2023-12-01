bac8349c62fd13618599a1bbd3429218465383b4
LSY <61727133+lsy-it-1995@users.noreply.github.com>
2023-11-30 22:19:53 -0600

    update Readme

--- LeetCode/00136. Single Number/136. Single Number.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int singleNumber(int[] nums) {
-        int ans = 0;
-        for(int item : nums){
-            ans = ans ^ item;
-        }
-        return ans;
-    }
-}
